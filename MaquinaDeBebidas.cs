5899eef143d10d21db16045ed28816465b0a4098
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2022-08-18 16:09:30 -0300

    Add files via upload

---
+++ MaquinaDeBebidas.cs
@@ -0,0 +1,158 @@
+﻿using System;
+namespace MaquinaDeBebidas
+{
+    public class Maquina
+    {
+        public string OpcaoMenu { get; set; }
+
+        public int Agua { get; set; }
+
+        public int Cafe { get; set; }
+
+        public double Suco { get; set; }
+
+        public int Refrigerante { get; set; }
+
+        public int Energetico { get; set; }
+
+        public double ValorTotal { get; set; }
+
+       
+
+        public void Comprar()
+        {
+            if (OpcaoMenu == "00" && Agua > 0)
+            {
+                Agua = 0;
+                ValorTotal = ValorTotal + 2.00;
+                Console.Clear();
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "0,0" && Agua == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "01" && Cafe > 0)
+            {
+                Cafe = 0;
+                ValorTotal = ValorTotal + 2.50;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "01" && Cafe == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "02" && Suco > 0)
+            {
+                Suco = 0;
+                ValorTotal = ValorTotal + 5.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "02" && Suco == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "03" && Refrigerante > 0)
+            {
+                Refrigerante = 0;
+                ValorTotal = ValorTotal + 5.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "03" && Refrigerante == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "04" && Energetico > 0)
+            {
+                Energetico = 0;
+                ValorTotal = ValorTotal + 10.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "04" && Energetico == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+
+        }
+
+        public void PrateleirasMenu()
+        {
+
+            string[,] prateleiras = new string[2, 5];
+            prateleiras[0, 0] = "Água - R$: 2,00 - [0,0]";
+            prateleiras[0, 1] = "Café - R$:2,50 - [0,1]";
+            prateleiras[0, 2] = "Suco - R$:5,00 - [0,2]";
+            prateleiras[0, 3] = "Refrigerante - R$:5,00 [0,3]";
+            prateleiras[0, 4] = "Energético - R$:10,00 - [0,4]";
+            prateleiras[1, 0] = "Repor - [1, 0]";
+            prateleiras[1, 1] = "Sair - [1, 1]";
+
+            for (int i = 0; i < prateleiras.GetLength(0); i++)
+            {
+                for (int j = 0; j < prateleiras.GetLength(1); j++)
+                {
+                    Console.WriteLine(prateleiras[i, j]);
+                }
+            }
+        }
+
+        public void Repor()
+        {
+            if (OpcaoMenu == "r")
+            {
+                Console.WriteLine("Digite o número do produto que deseja repor");
+                string produtoR = Console.ReadLine();
+
+                if (produtoR == "00" && Agua == 0)
+                {
+                    Agua++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "00" && Agua > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "01" && Cafe == 0)
+                {
+                    Cafe++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "01" && Cafe > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "02" && Suco > 0)
+                {
+                    Suco++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "02" && Suco > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "03" && Refrigerante == 0)
+                {
+                    Refrigerante++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "03" && Refrigerante > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "04" && Energetico == 0)
+                {
+                    Energetico++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "04" && Energetico > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+            }
+
+
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿/*string[,] estanteLivros = new string[3, 3];
+for (int linha = 0; linha < estanteLivros.GetLength(0); linha++)
+{
+    for (int coluna = 0; coluna < estanteLivros.GetLength(1); coluna++)
+    {
+        Console.WriteLine($"Digite o seu livro no indice linha {linha} coluna {coluna}: ");
+        estanteLivros[linha, coluna] = Console.ReadLine();
+        Console.WriteLine("\n");
+    }
+}
+for (int linha = 0; linha < estanteLivros.GetLength(0); linha++)
+{
+    for (int coluna = 0; coluna < estanteLivros.GetLength(1); coluna++)
+    {
+        Console.WriteLine(estanteLivros[linha, coluna]);
+        Console.WriteLine("\n");
+    }
+}
+
+
+string[,] campoMinado = new string[2, 2];
+campoMinado[0, 0] = "B";
+campoMinado[0, 1] = "B";
+campoMinado[1, 0] = "L";
+campoMinado[1, 1] = "L";
+for (int linhas = 0; linhas < campoMinado.GetLength(0); linhas++)
+{
+    for (int colunas = 0; colunas < campoMinado.GetLength(1); colunas++)
+    {
+        Console.Write($"{campoMinado[linhas, colunas]}   ");
+    }
+    Console.WriteLine();
+}
+
+*/
+
+using System;
+using MaquinaDeBebidas;
+
+Maquina maquina = new Maquina();
+
+maquina.Agua = 1;
+maquina.Cafe = 1;
+maquina.Suco = 1;
+maquina.Refrigerante = 1;
+maquina.Energetico = 1;
+bool loop = true;
+
+while (loop)
+{
+ 
+    Console.WriteLine("Escolha uma das opções do menu:");
+    maquina.PrateleirasMenu();
+    maquina.OpcaoMenu = Console.ReadLine();
+
+    if (maquina.OpcaoMenu == "00")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "01")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "02")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "03")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "04")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+
+    if (maquina.OpcaoMenu == "10")
+    {
+        Console.Clear();
+        maquina.Repor();
+    }
+    if (maquina.OpcaoMenu == "11")
+    {
+        Console.Clear();
+        Console.WriteLine($"Obrigado por usar nossa máquina. Seu total gasto foi de: {maquina.ValorTotal}");
+        break;
+
+    }
+}
